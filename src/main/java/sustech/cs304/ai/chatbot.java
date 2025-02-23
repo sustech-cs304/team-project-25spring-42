57fa766a4227b3bac6493025f4365584643807b3
Ruizhe Pang <pawn051028@gmail.com>
2025-02-23 21:45:02 +0800

    chatbot file

--- src/main/java/sustech/cs304/ai/chatbot.java
+++ src/main/java/sustech/cs304/ai/chatbot.java
@@ -1,2 +1,13 @@
 package sustech.cs304.ai;
 
+public abstract class Chatbot {
+    protected String botName;
+    protected String apiKey;
+
+    public Chatbot(String botName, String apiKey) {
+        this.botName = botName;
+        this.apiKey = apiKey;
+    }
+
+    public abstract String getResponse(String message);
+}
