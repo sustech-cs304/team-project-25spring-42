9cd7c76d376bab32ab1ef68c85084373f95c08aa
Ruizhe Pang <pawn051028@gmail.com>
2025-02-25 11:09:52 +0800

    seperate the editor pane and controller

--- src/main/java/sustech/cs304/IDE/IDEController.java
+++ src/main/java/sustech/cs304/IDE/IDEController.java
@@ -7,25 +7,11 @@ import eu.mihosoft.monacofx.MonacoFX;
 
 public class IDEController {
     
-    @FXML
-    private AnchorPane editorPane;
-
-    @FXML
-    private void initialize() {
-        EditorController editorController = new EditorController();
-        MonacoFX monacoFX = editorController.getMonacoFX();
-        monacoFX.prefWidthProperty().bind(editorPane.widthProperty());
-        monacoFX.prefHeightProperty().bind(editorPane.heightProperty());
-        editorPane.getChildren().add(monacoFX);
-        monacoFX.getEditor().getDocument().setText(
-                "#include <stdio.h>\n" +
-                "int main() {\n" +
-                "   // printf() displays the string inside quotation\n" +
-                "   printf(\"Hello, World!\");\n" +
-                "   return 0;\n" +
-                "}");
-        monacoFX.getEditor().setCurrentLanguage("c");
-        monacoFX.getEditor().setCurrentTheme("vs-dark");
-    }
+    // @FXML
+    // private AnchorPane editorPane;
+    //
+    // @FXML
+    // private void initialize() {
+    // }
 
 }
