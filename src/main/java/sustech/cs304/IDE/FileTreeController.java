ad66edab3c6c6ffc1c885f3f3002cbaa1187098f
Ruizhe Pang <pawn051028@gmail.com>
2025-02-25 20:28:36 +0800

    open file  by file tree

--- src/main/java/sustech/cs304/IDE/FileTreeController.java
+++ src/main/java/sustech/cs304/IDE/FileTreeController.java
@@ -5,19 +5,22 @@ import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
-
+import javafx.scene.control.TreeCell;
+import java.awt.Desktop;
 import java.io.File;
+import java.io.IOException;
 
 public class FileTreeController {
 
     @FXML
-    private TreeView<String> treeView;
+    private TreeView<FileTreeNode> treeView;
 
-    private TreeItem<String> rootItem;
+    private TreeItem<FileTreeNode> rootItem;
 
     @FXML
     private void initialize() {
-        rootItem = new TreeItem<>("Root");
+        FileTreeNode rootNode = new FileTreeNode("Folder", null);
+        rootItem = new TreeItem<>(rootNode);
         rootItem.setExpanded(true);
 
         treeView.setRoot(rootItem);
@@ -30,19 +33,32 @@ public class FileTreeController {
         Stage primaryStage = (Stage) treeView.getScene().getWindow();
 
         File selectedDirectory = directoryChooser.showDialog(primaryStage);
-        rootItem.setValue(selectedDirectory.getName());
         if (selectedDirectory != null) {
+            rootItem.setValue(new FileTreeNode(selectedDirectory.getName(), selectedDirectory.getAbsolutePath()));
             rootItem.getChildren().clear();
             buildFileTree(rootItem, selectedDirectory);
         }
         rootItem.setExpanded(true);
+
+        // 为 TreeView 设置鼠标点击事件
+        treeView.setOnMouseClicked(event -> {
+            TreeItem<FileTreeNode> selectedItem = treeView.getSelectionModel().getSelectedItem();
+            if (selectedItem != null) {
+                FileTreeNode node = selectedItem.getValue();  // 获取 FileTreeNode 对象
+                File selectedFile = new File(node.getPath());  // 通过完整路径查找文件
+                if (selectedFile.exists()) {
+                    openFile(selectedFile);
+                }
+            }
+        });
     }
 
-    private void buildFileTree(TreeItem<String> parentItem, File directory) {
+    private void buildFileTree(TreeItem<FileTreeNode> parentItem, File directory) {
         File[] files = directory.listFiles();
         if (files != null) {
             for (File file : files) {
-                TreeItem<String> fileItem = new TreeItem<>(file.getName());
+                FileTreeNode node = new FileTreeNode(file.getName(), file.getAbsolutePath());
+                TreeItem<FileTreeNode> fileItem = new TreeItem<>(node);
 
                 if (file.isDirectory()) {
                     buildFileTree(fileItem, file);
@@ -52,4 +68,58 @@ public class FileTreeController {
         }
     }
 
+    private File findFileByName(String name, File parentDir) {
+        File[] files = parentDir.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    File found = findFileByName(name, file);
+                    if (found != null) {
+                        return found;
+                    }
+                } else if (file.getName().equals(name)) {
+                    return file;
+                }
+            }
+        }
+        return null;
+    }
+
+    // 打开文件的方法
+    private void openFile(File file) {
+        if (file.isDirectory()) {
+            return;
+        } else {
+            try {
+                Desktop.getDesktop().open(file);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+}
+
+class FileTreeNode {
+    private String name;   // 文件名
+    private String path;   // 文件完整路径
+
+    public FileTreeNode(String name, String path) {
+        this.name = name;
+        this.path = path;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    @Override
+    public String toString() {
+        return name;  // 在树形视图中显示文件名
+    }
 }
