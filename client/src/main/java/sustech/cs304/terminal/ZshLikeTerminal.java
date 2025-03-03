7aca6ffb004d16556427b44c8d41da1b87982741
Ylers <144506679+Ylers13@users.noreply.github.com>
2025-03-03 14:58:23 +0800

    terminal zsh

---
+++ client/src/main/java/sustech/cs304/terminal/ZshLikeTerminal.java
@@ -0,0 +1,29 @@
+package sustech.cs304.terminal;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ZshLikeTerminal extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // load fxml file
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/terminal.fxml"));
+        Parent root = loader.load();
+
+
+        primaryStage.setTitle("Zsh-like Terminal");
+
+
+        Scene scene = new Scene(root, 600, 400);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
