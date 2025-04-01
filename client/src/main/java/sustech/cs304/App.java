6966b84fed0edbf68ad4ca3c4c47ae4c02f38e30
Ruizhe Pang <pawn051028@gmail.com>
2025-04-01 15:44:19 +0800

    Terminal UI

--- client/src/main/java/sustech/cs304/App.java
+++ client/src/main/java/sustech/cs304/App.java
@@ -16,7 +16,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         // URL fxmlUrl = getClass().getResource("/fxml/login.fxml");
-        URL fxmlUrl = getClass().getResource("/fxml/IDE/IDE.fxml");
+        URL fxmlUrl = getClass().getResource("/fxml/terminal.fxml");
         if (fxmlUrl == null) {
             System.err.println("FXML file not found! Check the path and ensure the file is in the resources folder.");
             return;
