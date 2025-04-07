933debe96e1076c0e643e5ce260eb09777542cc7
XianzhiTang001 <12312910@mail.sustech.edu.cn>
2025-04-07 17:15:02 +0800

    Add Class "Assignment" and Class "Course"

---
+++ client/src/main/java/sustech/cs304/classroom/Assignment.java
@@ -0,0 +1,9 @@
+package sustech.cs304.classroom;
+
+public class Assignment {
+    private String name;
+    private LocalDate dueDate;
+    private String status;
+
+
+}
---
+++ client/src/main/java/sustech/cs304/classroom/Course.java
@@ -0,0 +1,10 @@
+package sustech.cs304.classroom;
+
+public class Course {
+    private User teacher;
+    private User[] students;
+    private Assignment[] assignments;
+
+
+
+}
