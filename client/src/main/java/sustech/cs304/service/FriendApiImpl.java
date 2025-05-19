31671a534a2b736bd9cd1c7959115c69d2e9016a
Ruizhe Pang <pawn051028@gmail.com>
2025-05-19 14:17:53 +0800

    markdown notes

---
+++ client/src/main/java/sustech/cs304/service/FriendApiImpl.java
@@ -0,0 +1,21 @@
+package sustech.cs304.service;
+
+import java.util.List;
+
+public class FriendApiImpl implements FriendApi {
+    @Override
+    public List<String> getFriends(String userId) {
+        // TODO: Implement the logic to get the list of friends for a user
+        return null;
+    }
+
+    @Override
+    public boolean addFriend(String userId, String friendId) {
+        return false;
+    }
+
+    @Override
+    public boolean removeFriend(String userId, String friendId) {
+        return false;
+    }
+}
