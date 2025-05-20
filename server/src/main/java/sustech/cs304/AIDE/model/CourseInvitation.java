b59b3c82eb34e75e6ecf3189087402f57b1516df
Ruizhe Pang <pawn051028@gmail.com>
2025-05-20 22:58:48 +0800

    fix bug

--- server/src/main/java/sustech/cs304/AIDE/model/CourseInvitation.java
+++ server/src/main/java/sustech/cs304/AIDE/model/CourseInvitation.java
@@ -1,12 +1,18 @@
 package sustech.cs304.AIDE.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "course_invitation")
 public class CourseInvitation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private Long courseId;
     private String userId;
 
