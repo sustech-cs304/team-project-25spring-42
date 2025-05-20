14a2cc229287baf8f213f6b9663578310afc9f3c
Ruizhe Pang <pawn051028@gmail.com>
2025-05-20 20:55:46 +0800

    some fix

--- server/src/main/java/sustech/cs304/AIDE/repository/EnrollmentRepository.java
+++ server/src/main/java/sustech/cs304/AIDE/repository/EnrollmentRepository.java
@@ -1,10 +1,13 @@
 package sustech.cs304.AIDE.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+
 import java.util.Optional;
 import java.util.List;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import sustech.cs304.AIDE.model.Enrollment;
 
@@ -14,6 +17,9 @@ public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
     @Query("SELECT e.courseId FROM Enrollment e WHERE e.userId = :userId")
     List<Long> findCourseIdByUserId(@Param("userId") String userId);
 
+
+    @Modifying
+    @Transactional
     @Query("DELETE FROM Enrollment e WHERE e.courseId = :courseId")
     void deleteByCourseId(@Param("courseId") Long courseId);
 }
